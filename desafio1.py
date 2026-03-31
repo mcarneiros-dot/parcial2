a47ab1a9e126b3404fcda918f72ac9585aed4edd
mcarneiros-dot <matheus.carneiro.silverio@escola.pr.gov.br>
2026-03-31 08:23:58 -0300

    Create desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,8 @@
+# Solicita o primeiro número ao usuário e o converte para float
+num1 = float(input("Digite o primeiro número: ")) 
+# Solicita o segundo número ao usuário e o converte para float
+num2 = float(input("Digite o segundo número: ")) 
+# Realiza a soma entre os dois números armazenados
+soma = num1 + num2 
+# Exibe o resultado da soma na tela para o usuário
+print("A soma dos números é:", soma)
