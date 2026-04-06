6814283f2038c8e78fe29aec49e82d871bc1a2f1
mcarneiros-dot <matheus.carneiro.silverio@escola.pr.gov.br>
2026-04-06 07:50:18 -0300

    Create desafio4.py

---
+++ desafio4.py
@@ -0,0 +1,26 @@
+# Calculadora simples
+
+print("Escolha a operação:")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+
+opcao = input("Digite a opção: ")
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+if opcao == "1":
+    print("Resultado:", n1 + n2)
+elif opcao == "2":
+    print("Resultado:", n1 - n2)
+elif opcao == "3":
+    print("Resultado:", n1 * n2)
+elif opcao == "4":
+    if n2 != 0:
+        print("Resultado:", n1 / n2)
+    else:
+        print("Erro: divisão por zero!")
+else:
+    print("Opção inválida!")
